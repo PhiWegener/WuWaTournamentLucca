dbeebe8f59d4b3a8868912a53791955f03347b8b
pwegener <pwegener@circular.de>
2026-02-09 17:01:20 +0100

    default pk

--- wuwa-django/config/settings.py
+++ wuwa-django/config/settings.py
@@ -148,3 +148,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 50,
 }
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
