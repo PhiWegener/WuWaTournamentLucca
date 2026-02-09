02a48e3d3c82505a2353a28a162b13b6a37701e8
pwegener <pwegener@circular.de>
2026-02-09 17:30:22 +0100

    debug

--- wuwa-django/core/views.py
+++ wuwa-django/core/views.py
@@ -10,7 +10,7 @@ from .models import Tournament, Match, BossTime, Boss, Player, UserRole, MatchSi
 from .forms import HostMatchCreateForm, HostMatchWinnerForm, PlayerTimeSubmitForm, DraftActionForm, MatchTimeSubmitForm, BanConfirmForm, PickConfirmForm
 from .permissions import requireRole, requireLogin
 from .ws import broadcastDraftUpdate
-from .draft import _getCurrentBanSlot, _getCurrentPickSlot, BAN_COUNT, PICK_COUNT
+from .draft import _getCurrentBanSlot, _getCurrentPickSlot, BAN_COUNT, PICK_COUNT, buildDraftContext
 
 def home(request):
     return render(request, "core/home.html")
