5f2721dd6b36c21e87923c6e5b03eae62529fd19
pwegener <pwegener@circular.de>
2026-02-09 14:54:42 +0100

    added allowed hosts

--- wuwa-django/config/settings.py
+++ wuwa-django/config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-+feelvju%e36n(jz(si^(k*!1-e2havdy0bf)kuaagptxfc#7#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["192.168.2.128"]
 
 # Application definition
 
