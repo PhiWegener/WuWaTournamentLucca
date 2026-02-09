817a116321de6bda71dbccdfd7d388ea1d95db41
pwegener <pwegener@circular.de>
2026-02-09 17:26:25 +0100

    websocket troubleshooten

--- wuwa-django/config/asgi.py
+++ wuwa-django/config/asgi.py
@@ -1,13 +1,18 @@
 import os
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-import core.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+# 1) Erst Django ASGI App initialisieren (lädt Apps/Registry)
 django_asgi_app = get_asgi_application()
+
+# 2) Danach erst routing importieren (dann dürfen consumers/models geladen werden)
+import core.routing  # noqa: E402
+
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
